eb6e09cf156d6da6e619e30a97a13a3211328563
atulcp2001 <atulcp2001@gmail.com>
2023-08-01 23:53:07 -0700

    commit 11 - cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,4 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    "https://www.eternity-solutions.com",
-    "https://eternity-solutions.com",
-    "https://www.google.com",
     "https://deploy3-mu24.onrender.com/"
 ];
 
