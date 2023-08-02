da30809a161b445a5c5fa43cbf6f943baef60223
atulcp2001 <atulcp2001@gmail.com>
2023-08-01 23:43:29 -0700

    commit 10 - deployment changes

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     "https://www.eternity-solutions.com",
     "https://eternity-solutions.com",
-    "https://www.google.com"
+    "https://www.google.com",
+    "https://deploy3-mu24.onrender.com/"
 ];
 
 module.exports = allowedOrigins
